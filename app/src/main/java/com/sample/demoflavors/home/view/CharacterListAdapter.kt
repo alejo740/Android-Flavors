93e9b0570b55a9c7681dfd981bc19860dcc5fc4e
Edwin Cobos <alejo740@gmail.com>
2019-01-11 12:38:28 -0600

    Implemented RecyclerView list with toggle view

--- app/src/main/java/com/sample/demoflavors/home/view/CharacterListAdapter.kt
+++ app/src/main/java/com/sample/demoflavors/home/view/CharacterListAdapter.kt
@@ -1,19 +1,26 @@
 package com.sample.demoflavors.home.view
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.sample.demoflavors.R
 import com.sample.demoflavors.api.models.CharacterModel
+import kotlinx.android.synthetic.main.grid_character_item.view.*
+import kotlinx.android.synthetic.main.linear_character_item.view.*
 
 class CharacterListAdapter(val clickListener: (CharacterModel) -> Unit) :
     RecyclerView.Adapter<CharacterListAdapter.CharacterViewHolder>() {
 
     var items: ArrayList<CharacterModel> = ArrayList()
+    var linearView: Boolean = true
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CharacterViewHolder {
-        val layout = LayoutInflater.from(p0.context).inflate(R.layout.linear_character_item, p0, false)
+        Log.i("FLAV", "CREATE...")
+        val layoutId = if(linearView) R.layout.linear_character_item else R.layout.grid_character_item
+        val layout = LayoutInflater.from(p0.context).inflate(layoutId, p0, false)
         return CharacterViewHolder(layout)
     }
 
@@ -22,6 +29,7 @@ class CharacterListAdapter(val clickListener: (CharacterModel) -> Unit) :
     }
 
     override fun onBindViewHolder(p0: CharacterViewHolder, p1: Int) {
+        Log.i("FLAV", "CREATE...")
         p0.bind(items[p1], clickListener)
     }
 
@@ -37,7 +45,20 @@ class CharacterListAdapter(val clickListener: (CharacterModel) -> Unit) :
     }
 
     class CharacterViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+        private var title = view.title_text
+        private var image = view.image_view
+
         fun bind(character: CharacterModel, clickListener: (CharacterModel) -> Unit) {
+
+            title?.let { it.text = character.title}
+
+            image?.let {
+                Glide.with(image.context)
+                    .load(character.photoUrl)
+                    .into(image)
+            }
+
             view.setOnClickListener {
                 clickListener(character)
             }
--- app/src/main/java/com/sample/demoflavors/home/view/MainActivity.kt
+++ app/src/main/java/com/sample/demoflavors/home/view/MainActivity.kt
@@ -3,8 +3,11 @@ package com.sample.demoflavors.home.view
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.sample.demoflavors.R
 import com.sample.demoflavors.api.models.CharacterModel
 import com.sample.demoflavors.app.App
@@ -17,6 +20,8 @@ class MainActivity : AppCompatActivity(), HomeView {
     @Inject
     lateinit var presenter: HomePresenter
 
+    private val gridLayout = GridLayoutManager(this, 1)
+
     private lateinit var adapterCharacterList: CharacterListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +32,8 @@ class MainActivity : AppCompatActivity(), HomeView {
     }
 
     private fun setupList() {
-        item_list.layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
+        item_list.layoutManager = gridLayout
+        item_list.setHasFixedSize(true)
         adapterCharacterList = CharacterListAdapter { characterModel -> openCharacterDetail(characterModel) }
         item_list.adapter = adapterCharacterList
     }
@@ -41,6 +47,36 @@ class MainActivity : AppCompatActivity(), HomeView {
     override fun onPause() {
         super.onPause()
         presenter.rxUnsubscribe()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.clear()
+        val inflater = MenuInflater(this)
+        inflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item!!.itemId) {
+            R.id.toggle_view -> {
+                toggleView()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun toggleView() {
+        val scrollPos = (item_list.layoutManager as LinearLayoutManager).findFirstCompletelyVisibleItemPosition()
+        if(gridLayout.spanCount == 1) {
+            adapterCharacterList.linearView = false
+            gridLayout.spanCount = 2
+        }else{
+            adapterCharacterList.linearView = true
+            gridLayout.spanCount = 1
+        }
+        item_list.adapter = adapterCharacterList
+        item_list.scrollToPosition(scrollPos)
     }
 
     private fun openCharacterDetail(characterModel: CharacterModel) {
